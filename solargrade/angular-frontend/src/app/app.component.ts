0cf32d0109ff81d3c4666f42104ea9b70fe1636b
schimmy <colinschimmelfing@gmail.com>
2022-10-19 10:59:44 -0400

    hit the local API instead of using mock API data

--- solargrade/angular-frontend/src/app/app.component.ts
+++ solargrade/angular-frontend/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { IssueBoxComponent } from './issue-box/issue-box.component'
 import { InspectionTableComponent } from './inspection-table/inspection-table.component'
 import { Inspection } from './inspection'
-import { INSPECTIONS } from './mock-inspections'
 
 @Component({
   selector: 'app-root',
@@ -12,29 +12,47 @@ import { INSPECTIONS } from './mock-inspections'
 
 export class AppComponent implements OnInit {
   title = 'SolarGrade - Colin';
-  inspectionsInput: Inspection[] = INSPECTIONS;
+  inspectionsInput: Inspection[] = [];
+  // TODO: probably make this less FakeSolar-specific, allow for other companies
+  api_url = '/inspections?company=FakeSolar'
+  error = ''
 
   // TODO put this into a test
+  numInspectionsWarning = 0;
+  numIssuesWarning = 0;
   numInspectionsCritical = 0;
   numIssuesCritical = 0;
 
+  fetch_inspections_from_api() {
+    this.http.get<any>(this.api_url).subscribe({
+      next: data => {
+        this.inspectionsInput = data['data'] as Inspection[];
+        this.recalculateIssueBoxes()
+      },
+      error: error => {
+        console.error("error: ", error.message)
+        this.error = "Error retrieving data: " + error.message
+      }
+    })
+  }
 
-
-  constructor() {
-    // TODO: add in getter from API backend
-
+  recalculateIssueBoxes() {
     for (var insp of this.inspectionsInput) {
       var warns = insp['issuesWarningCount'];
       var crits = insp['issuesCriticalCount'];
+      if (warns > 0) {
+        this.numInspectionsWarning += 1
+        this.numIssuesWarning += warns
+      }
       if (crits > 0) {
         this.numInspectionsCritical += 1;
         this.numIssuesCritical += crits;
       }
-      //if (warns > 0) {
-      //  numInspectionsWarning += 1
-      //  numIssuesWarning += crits
-      //}
     }
+  }
+
+  constructor(private http: HttpClient) {
+    this.fetch_inspections_from_api()
   }
 
   ngOnInit(): void {
--- solargrade/angular-frontend/src/app/app.module.ts
+++ solargrade/angular-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { IssueBoxComponent } from './issue-box/issue-box.component';
@@ -12,7 +13,8 @@ import { InspectionTableComponent } from './inspection-table/inspection-table.co
     InspectionTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
